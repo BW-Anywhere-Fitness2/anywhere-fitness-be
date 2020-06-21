f776c6a0f3e1a0386ea878190b806295e6198ea4
Gabriel Romero <gabester78@gmail.com>
2020-06-21 18:13:39 -0500

    ugh

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,11 @@
+// const knex = require("knex");
+
+// const config = require("../knexfile.js");
+
+// const enviroment = process.env.DB_ENV || "development";
+// module.exports = knex(config[enviroment]);
+
 const knex = require("knex");
+const knexConfig = require("../knexfile.js");
 
-const config = require("../knexfile.js");
-
-const enviroment = process.env.DB_ENV || "development";
-module.exports = knex(config[enviroment]);
+module.exports = knex(knexConfig.development);
